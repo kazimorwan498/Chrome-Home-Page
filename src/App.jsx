0b8ae0412f26927fbdc82b60b939420f04d393b3
Kazi Morwan <kazimdmorwan498@gmail.com>
2025-11-10 23:44:37 +0600

    fix bg problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import CurrentTime from "./components/CurrentTime";
 import Header from "./components/Header";
+import bg from "./assets/bg.jpg";
 
 const App = () => {
   return (
-    <div className="bg-[url(bg.jpg)] bg-cover bg-center h-screen">
+    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${bg})` }}>
       <div className="flex justify-between">
         <CurrentTime />
         <Header />
