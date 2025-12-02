3cdeddb731c15a1e9b3b24bb1436dd9b30f3c702
kazimorwan498 <kazimdmorwan498@gmail.com>
2025-12-03 00:19:09 +0600

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./Components/MyHeader";
+import MyHeader from "./Components/MyHeader";
 import SearchBar from "./Components/SearchBar";
 import QuickLinks from "./Components/QuickLinks";
 import bg from "./assets/bg.jpg";
@@ -6,7 +6,7 @@ import bg from "./assets/bg.jpg";
 const App = () => {
   return (
     <div className="bg-cover bg-center bg-no-repeat h-screen flex flex-col relative" style={{ backgroundImage: `url(${bg})` }}>
-      <Header />
+      <MyHeader />
       <div className="flex-1 flex flex-col items-center justify-center px-4 pb-20 md:pb-32">
         <div className="mb-12 md:mb-16">
           <h1 className="text-white text-5xl md:text-7xl font-normal mb-2 select-none">Google</h1>
